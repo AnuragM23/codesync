ebe7bcb3ea6a6173d4b8a6f59ef23dfb4690085f
AnuragM23 <anuragmahato006@gmail.com>
2025-04-07 20:04:19 +0530

    navbar built

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,7 @@ import { SignInButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="m-10">
-      <SignInButton>
-        <Button>
-          Login
-        </Button>
-      </SignInButton>
+      home page
     </div>
   );
 }
