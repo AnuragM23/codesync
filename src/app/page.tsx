e5ff8cfa8ded64713e27c9614f39fa0f0a3df9cc
AnuragM23 <anuragmahato006@gmail.com>
2025-04-07 11:44:26 +0530

    convex setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,8 @@
-import { Button } from "@/components/ui/button";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="m-10">
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      Home Page
     </div>
   );
 }
